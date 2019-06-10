9a0316b17a4efa00a0143e71b31ae405858a462c
Dmitry Andriyanov <dima117a@yandex-team.ru>
2019-06-10 09:43:38 +0300

    lib → src

--- test/matchPath.test.ts
+++ test/matchPath.test.ts
@@ -1,7 +1,7 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
-import { matchPath } from '../lib/matchPath';
+import { matchPath } from '../src/matchPath';
 
 describe('matchPath', () => {
   it('invalid url => null', () => {
